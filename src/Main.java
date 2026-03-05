87f95f54b300a388ea1ec07f44feb506ea27b12e
prasannathiruvedi <prasannathiruvedi810@gmail.com>
2026-03-05 14:56:51 +0530

    Prasanna Thiruvedi added UC4

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String word = "madam";
+
+        // Convert string to character array
+        char[] characters = word.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is not a Palindrome.");
+        }
+    }
+}
