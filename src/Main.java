38d20b5927b26572ad58488f71d6044094f6da3c
prasannathiruvedi <prasannathiruvedi810@gmail.com>
2026-03-05 15:05:33 +0530

    Prasanna Thiruvedi added UC5

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Declare and initialize the input string
+        String input = "noon";
+
+        // Create a Stack to store characters
+        Stack<Character> stack = new Stack<>();
+
+        // Push each character of the string into the stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        // Assume palindrome initially
+        boolean isPalindrome = true;
+
+        // Iterate again through original string
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is not a Palindrome.");
+        }
+    }
+}
